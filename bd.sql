7391e35c2092030cfc07de2e5784b1550b0e866f
unknown <giogoncalvespadua13@gmail.com>
2026-03-13 09:18:34 -0300

    Comecando projeto

---
+++ bd.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE aula_mvc;
+USE aula_mvc;
+
+CREATE TABLE IF NOT EXISTS produtos (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+descricao TEXT,
+preco DECIMAL(10,2) NOT NULL,
+quantidade INT NOT NULL
+);
