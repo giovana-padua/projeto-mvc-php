dfe529a10de7527dd6b2d8d74389bf00ced51d55
unknown <giogoncalvespadua13@gmail.com>
2026-03-20 10:59:04 -0300

    Aula 20 de mar

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -11,8 +11,9 @@
         public function all()
         {
             // retornar todos os produtos
-            
             // Model acessa o BD, faz um SELECT e retorna para o controller
+            $stmt = $this->conn->query("SELECT * FROM {$this->table}");
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
         
         public function find($id)
@@ -23,6 +24,8 @@
         public function create($dados)
         {
             // inserir produto
+            $stmt = $this->conn->query("INSERT INTO produtos (nome, preco, quantidade) VALUES ($dados[0], $dados[1], $dados[2], $dados[3]);");
+
         }
         public function update($id, $dados)
         {
--- app/controllers/ProdutoController.php
+++ app/controllers/ProdutoController.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__ . '/../../core/Database.php';
     require_once __DIR__ . '/../models/Produto.php';
+    // Controlador é o intermediário entre View e Model
     class ProdutoController
     {
         private $produtoModel;
@@ -13,18 +14,34 @@
 
         public function index()
         {
-            return $this->produtoModel->all();
+            $produtos = $this->produtoModel->all();
             // Chama a view com os dados recebidos do model
+            require __DIR__ . '/../Views/produtos/index.php';
         }
 
         public function create()
         {
-        // mostrar formulário
+            // mostrar formulário
+            //1° pedir informações para o Model
+
+            require __DIR__ . '/../Views/produtos/create.php';
+            
         }
 
         public function store()
         {
-        // salvar produto
+            // salvar produto
+            if ($_SERVER('REQUEST_METHOD') == 'POST')
+            {
+                $nomeProd = $_POST('nome'); 
+                $descricaoProd = $_POST('descricao'); 
+                $precoProd = $_POST('preco'); 
+                $quantidadeProd = $_POST('quantidade'); 
+
+                $dados = [$nomeProd, $descricaoProd, $precoProd, $quantidadeProd];
+
+                $this->produtoModel($dados);
+            }
         }
 
         public function edit()
